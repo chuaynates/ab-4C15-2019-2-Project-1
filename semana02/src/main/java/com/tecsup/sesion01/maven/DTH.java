4db63db5e7e898cb28b24d7c42a910db26e28a2b
Diego <diego.torres.h@tecsup.edu.pe>
2019-08-27 17:33:01 -0500

    New class

--- semana02/src/main/java/com/tecsup/sesion01/maven/DTH.java
+++ semana02/src/main/java/com/tecsup/sesion01/maven/DTH.java
@@ -1,13 +1,3 @@
-package com.tecsup.sesion01.maven;
-
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class DTH {
+    
 }
